0e2c2b5e4747f6390b0f351fa802917e1b17a71d
ouyangduning <ouyangduning@jd.com>
2020-09-23 11:35:29 +0800

    尝试 retrofit

---
+++ src/main/java/com/ouyang/lesson/http/HttpApi.java
@@ -0,0 +1,8 @@
+package com.ouyang.lesson.http;
+
+/**
+ * @author ouyangduning
+ * @date 2020/9/23 10:24
+ */
+public class HttpApi {
+}
---
+++ src/test/java/com/ouyang/lesson/http/HttpApiTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+/**
+ * @author ouyangduning
+ * @date 2020/9/23 10:28
+ */
+public class HttpApiTest extends TestCase {
+
+}
